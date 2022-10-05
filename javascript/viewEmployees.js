dab972e65d1cb901999034cdc5015b6340dbf2ba
OliverBark <oliver.bark@icloud.com>
2022-10-05 11:15:38 +0200

    Prøver ihærdigt at fikse noget her

--- javascript/viewEmployees.js
+++ javascript/viewEmployees.js
@@ -1,9 +1,24 @@
 
-async function viewEmployees(){
-    const url = ''
-    const response = await fetch(url)
-    const employeeData = await response.json();
 
-    
 
-}
+    async function fetchData() {
+        let response = await fetch("https://kinoxp-na.azurewebsites.net/api/v1/employees");
+        let data = await response.json();
+        console.log(JSON.stringify(data));
+    }
+    fetchData();
+
+
+    async function loadIntoTable(url,table){
+        const tableHead = table.querySelector("thead");
+        const tableBody = table.querySelector("tbody");
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        
+        document.getElementById('name').innerText = Navn;
+       
+    }
+
+    loadIntoTable("https://kinoxp-na.azurewebsites.net/api/v1/employees", document.querySelector("table"))
