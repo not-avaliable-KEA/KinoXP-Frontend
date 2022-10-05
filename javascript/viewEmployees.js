629dbda1d0f93d14fe4df414233a34484bff826c
Eliot996 <math10t6@stud.kea.dk>
2022-10-05 13:03:51 +0200

    KIN-31 simple view of all the employees in the sys

--- javascript/viewEmployees.js
+++ javascript/viewEmployees.js
@@ -1,24 +1,38 @@
 
+        const table_body = document.getElementById('table-body');
+        const url = "https://kinoxp-na.azurewebsites.net/api/v1/employees";
 
 
     async function fetchData() {
-        let response = await fetch("https://kinoxp-na.azurewebsites.net/api/v1/employees");
+        let response = await fetch(url);
         let data = await response.json();
-        console.log(JSON.stringify(data));
+        let list = Array.from(data);
+
+        list.forEach((element) => makeNewEmployee(element));
     }
     fetchData();
 
+    function makeNewEmployee(element) {
+        let row = document.createElement("tr");
+        row.id = "id-" + element.id;
 
-    async function loadIntoTable(url,table){
-        const tableHead = table.querySelector("thead");
-        const tableBody = table.querySelector("tbody");
-        const response = await fetch(url);
-        const data = await response.json();
+        row.innerHTML = "<td>" + element.id +  "</td>" + 
+                        "<td>" + element.name + "</td>" +
+                        "<td>" + element.role + "</td>" +
+                        "<td>" + element.email + "</td>" +
+                        "<td>" + element.telephone + "</td>" +
+                        "<td>" + element.id + "</td>";
 
-        console.log(data);
-        
-        document.getElementById('name').innerText = Navn;
-       
+        table_body.appendChild(row);
     }
 
-    loadIntoTable("https://kinoxp-na.azurewebsites.net/api/v1/employees", document.querySelector("table"))
+    /*
+        {
+        "id": 1,
+        "name": "new",
+        "role": "role",
+        "email": "email",
+        "telephone": "phone",
+        "username": "username3"
+    }
+    */
