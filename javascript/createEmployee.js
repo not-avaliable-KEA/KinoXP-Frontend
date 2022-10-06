e5bedf5038ed5b83e6d6dcd33b61bfb90330b2f4
Eliot996 <math10t6@stud.kea.dk>
2022-10-06 10:42:59 +0200

    KIN-29 KIN-30 made so it can perform double duty for both update and create, still missing for push

--- javascript/createEmployee.js
+++ javascript/createEmployee.js
@@ -1,9 +1,53 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id')
+
+//const url = "https://kinoxp-na.azurewebsites.net/api/v1/employees";
+const url = "http://localhost:8080/api/v1/employees";
+
+
+function checkIfUpdate() {
+
+    console.log(id);
+    if (id != null && id > 0) {
+        showEmployee();
+    } else {
+        id = null;
+    }
+}
+checkIfUpdate();
+
+
 
 async function showEmployee(){
-    const url = ''
-    const response = await fetch(url)
+    const response = await fetch(url + "/" + id);
     const employeeData = await response.json();
 
-    
+    document.getElementById("name").value = employeeData.name;
+    document.getElementById("email").value = employeeData.email;
+    document.getElementById("telephone").value = employeeData.telephone;
+    document.getElementById("username").value = employeeData.username;
+    document.getElementById("role").value = employeeData.role;
+    document.getElementById("button").innerHTML = 'Opdater';
 
 }
+
+
+async function sendData() {
+    let form = new FormData(document.getElementById("form"));
+
+    const data = Object.fromEntries(form.entries());
+}
+
+
+
+/*
+        {
+        "id": 1,
+        "name": "new",
+        "role": "role",
+        "email": "email",
+        "telephone": "phone",
+        "username": "username3"
+        }
+    */
