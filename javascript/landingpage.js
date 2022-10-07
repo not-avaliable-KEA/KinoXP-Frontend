377998fa587260d97fdb9a338a9e2b0d3d3175e2
H4ppyN4p <tobi5703@stud.kea.dk>
2022-10-07 13:15:55 +0200

    Working carousel of movies split in genres

--- javascript/landingpage.js
+++ javascript/landingpage.js
@@ -4,61 +4,60 @@ async function getMoviesAndGenres(){
     const moviesUrl= "http://localhost:8080/api/v1/movies";
     const movieResponse = await fetch(moviesUrl);
     const movieData = await movieResponse.json();
-    console.log(movieData);
-
-    const genreUrl = "http://localhost:8080/api/v1/movies?genre";
-    const genreResponse = await fetch(genreUrl);
-    const genreData = await genreResponse.json();
-    console.log(genreData);
 
     const movieListe = Array.from(movieData);
+    const genreMap = new Map();
 
-    function reducer(prev, curr){
-        const currentGenre = curr.genre;
-        console.log(currentGenre, prev[currentGenre])
-        if(!prev[currentGenre]){
-            prev[currentGenre] =[]
-        }
-
-        prev[currentGenre].push(curr)
-
-        return prev;
+    for(let i=0; i<movieListe.length; i++){
+      genreMap.set(movieListe[i].genre, movieListe[i].genre)
     }
-    
-    let genreList = movieListe.reduce(reducer, {}) 
 
-    console.log(genreList);
+    const GenreValues = Array.from(genreMap.values());
 
-    const genres = Object.keys(genreList)
-    const carousselpage = document.getElementById("movies");
+    for(let i=0; i<genreMap.size; i++){
+      let element = document.createElement("div")
 
-    
-    for(const genre of genres){
-        const moviesInGenre = genreList[genre]
-        for(const movie of moviesInGenre){
+      document.getElementById("carousel-inner").appendChild(element)
 
-            carousselpage.innerHTML +=
+      if (i === 0){
+      element.className = "carousel-item active"
+      element.id = `carousel-item-${GenreValues[i]}`
+    } else {
+      element.className = "carousel-item"
+      element.id = `carousel-item-${GenreValues[i]}`
+    }
 
-            `<div class="col-6 col-md-4 col-xl-2">
-            <div class="card" style="width: 100%;">
-              <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${movie.name}</h5>
-                <p class="card-text">
-                
-                <div><p>Genre: </p> ${movie.genre}</div>
-                <div><p>Længde: </p>${movie.length}</div>
-                <div><p>Skuespillere: </p>${movie.actors}</div> 
+      let carouselContainer = document.createElement("div")
+      document.getElementById(`carousel-item-${GenreValues[i]}`).appendChild(carouselContainer)
+      carouselContainer.className = "carousel-container container-fluid"
+      carouselContainer.id = `carousel-container-${GenreValues[i]}`
 
-                </p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>`
+      let row = document.createElement("div")
+      document.getElementById(`carousel-container-${GenreValues[i]}`).appendChild(row)
+      row.className = "row"
+      row.id = `row-${GenreValues[i]}`
+
+      for (let ii = 0; ii < movieListe.length; ii++){
+
+        if (movieListe[ii].genre === GenreValues[i]){
+          let elem = document.createElement("div")
+
+            document.getElementById(`row-${GenreValues[i]}`).appendChild(elem)
+
+            elem.style.border = "2px solid red" 
+            elem.className = 'col-6 col-md-4 col-xl-2'
             
+            elem.innerHTML = `<div class="card"> 
+                                <img src="https://via.placeholder.com/150" class="card-img-top" />  
+                                <div class="card-body">  
+                                    <h5 class="card-title"> ` + movieListe[ii].name + `</h5> 
+                                    </div>
+                                </div>`
         }
+
+      }
+    
     }
-
-
+    
 }
 
