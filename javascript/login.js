d93fb3a70b310ba6b374e0facf105dbcdb547be8
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-10-12 13:21:17 +0200

    LOGIN VIRKER i js og html.

--- javascript/login.js
+++ javascript/login.js
@@ -1,26 +1,20 @@
-
-
-//import {config} from "./config.js";
-
-const url =  config.url + "/api/v1/employeeHomepage";
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let id = urlParams.get('id')
+//import {config} from './config';
+//const url = config.url + "api/v1/employees/login";
+//let id;
+const url = "http://localhost:8080/api/v1/employees/login";
 var attempt = 3; // Variable to count number of attempts.
 
+async function validateLogin(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if ( username == username && password == password){
-        alert ("Login successfully");
-        window.location = "/html/employeeHomepage.html"; // Redirecting to other page.
-        return true;
-    }
-    else{
+    if (await loginValidation(username, password) === true){
+        window.location = "../html/employeeHomepage.html"; // Redirecting to other page.
+
+    } else {
         attempt --;// Decrementing by one.
-        alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
+        alert("Du har nu "+attempt+" forsøg tilbage;");
+        // Disabling fields after 3 attempts.
         if( attempt == 0){
             document.getElementById("username").disabled = true;
             document.getElementById("password").disabled = true;
@@ -28,4 +22,23 @@ function validate(){
             return false;
         }
     }
+
+    async function loginValidation(username, password){
+        const values = {
+            "username": username,
+            "password": password
+        }
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        });
+        let data = await response.json();
+        return data;
+    }
 }
+
+
